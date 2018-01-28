01f15471de9b73ccf4a1f2cadd15705de45c3d46
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-01-28 20:20:14 +0600

    Add files via upload

---
+++ The Haunted House.cpp
@@ -0,0 +1,54 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sf scanf
+#define pf printf
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define u unsigned int
+
+int main (void)
+{
+    int t,len,i;
+    string str;
+
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> str;
+
+        string rev = "";
+
+        len = str.size()/2;
+
+        for (i=0; i<len; i++)
+            rev += str[i];
+
+        rev = string (rev.rbegin(),rev.rend());
+
+        cout << rev << endl;
+    }
+
+    return 0;
+}
