7c6c7eac62f56f349908cca8b1c00db15c6a2a73
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-01-28 20:22:30 +0600

    Add files via upload

---
+++ Magic Square.cpp
@@ -0,0 +1,44 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sf scanf
+#define pf printf
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define u unsigned int
+
+int main (void)
+{
+    int t,pos,n;
+
+    cin >> t;
+
+    for (pos=1; pos<=t; pos++)
+    {
+        cin >> n;
+
+        pf ("Case %d: %d\n",pos,n*(n*n+1)/2);
+    }
+
+    return 0;
+}
