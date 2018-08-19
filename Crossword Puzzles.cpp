29999b92c8ca6138baa7a768f82439c8c6604a8b
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-08-19 07:42:05 +0600

    Add files via upload

---
+++ Crossword Puzzles.cpp
@@ -0,0 +1,114 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir,
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sf scanf
+#define pf printf
+#define ssf sscanf
+#define spf sprintf
+#define fsf fscanf
+#define fpf fprintf
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define vi vector <int>
+#define pii pair <int,int>
+#define mii map <int,int>
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define U unsigned int
+#define endl "\n"
+#define bair_ho return 0
+#define shuru_kor_bhai int main (void)
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+int SetBit (int n, int x) { return n | (1 << x); }
+int ClearBit (int n, int x) { return n & ~(1 << x); }
+int ToggleBit (int n, int x) { return n ^ (1 << x); }
+bool CheckBit (int n, int x) { return (bool)(n & (1 << x)); }
+
+shuru_kor_bhai
+{
+    /*
+    	freopen ("input.txt","r",stdin);
+    	freopen ("output.txt","w",stdout);
+    */
+
+    int tc,pos,r,c,i,j;
+    char str[105][105];
+
+    sf ("%d",&tc);
+
+    for (pos=1; pos<=tc; pos++)
+    {
+        sf ("%d %d",&r,&c);
+
+        for (i=0; i<r; i++)
+            for (j=0; j<c; j++)
+                cin >> str[i][j];
+
+        vector <string> v;
+        string x;
+
+        for (i=0; i<r; i++)
+        {
+            for (j=0; j<c; j++)
+            {
+                if (str[i][j] != 'X')
+                    x.pb(str[i][j]);
+
+                if (str[i][j] == 'X' || j == c-1)
+                {
+                    if (x.size() >= 2)
+                        v.pb(x);
+
+                    x.clear();
+                }
+            }
+        }
+
+        for (j=0; j<c; j++)
+        {
+            for (i=0; i<r; i++)
+            {
+                if (str[i][j] != 'X')
+                    x.pb(str[i][j]);
+
+                if (str[i][j] == 'X' || i == r-1)
+                {
+                    if (x.size() >= 2)
+                        v.pb(x);
+
+                    x.clear();
+                }
+            }
+        }
+
+        sort (v.begin(),v.end());
+
+        pf ("Case %d: %s\n",pos,v[0].c_str());
+
+        v.clear();
+    }
+
+    bair_ho;
+}
