139d5c1c443aa4111f57d53d752cd50132509b1d
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-08-19 21:38:44 +0600

    Add files via upload

---
+++ Choto Chele and Dumb Numbers.cpp
@@ -0,0 +1,95 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir,
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ebar_shuru_kor_bhai int main (void)
+#define bair_ho return 0
+#define sf scanf
+#define pf printf
+#define ssf sscanf
+#define spf sprintf
+#define fsf fscanf
+#define fpf fprintf
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define vi vector <int>
+#define pii pair <int,int>
+#define mii map <int,int>
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define U unsigned int
+#define endl "\n"
+
+const int MOD = 1000000007;
+const int MAX = 100005;
+
+int SetBit (int n, int x) { return n | (1 << x); }
+int ClearBit (int n, int x) { return n & ~(1 << x); }
+int ToggleBit (int n, int x) { return n ^ (1 << x); }
+bool CheckBit (int n, int x) { return (bool)(n & (1 << x)); }
+
+bool prime[MAX];
+int arr[MAX];
+
+void sieve ()
+{
+    int i,j;
+
+    prime[0] = prime[1] = true;
+
+    for (i=4; i<MAX; i+=2)
+        prime[i] = true;
+
+    for (i=3; i*i<=MAX; i+=2)
+        if (!prime[i])
+            for (j=i*i; j<MAX; j+=2*i)
+                prime[j] = true;
+
+    for (i=2; i<MAX; i++)
+    {
+        if (!prime[i])
+            arr[i] = arr[i-1]+1;
+        else
+            arr[i] = arr[i-1];
+    }
+}
+
+ebar_shuru_kor_bhai
+{
+    /*
+    	freopen ("input.txt","r",stdin);
+    	freopen ("output.txt","w",stdout);
+    */
+
+    sieve ();
+
+    int tc,pos,i,a,b;
+
+    sf ("%d",&tc);
+
+    for (pos=1; pos<=tc; pos++)
+    {
+        sf ("%d %d",&a,&b);
+        pf ("Case %d: %d\n",pos,arr[b]-arr[a-1]);
+    }
+
+    bair_ho;
+}
